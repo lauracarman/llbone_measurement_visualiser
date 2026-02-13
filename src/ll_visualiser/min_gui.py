b92db740d7b78def301783e327a75975f1090bce
tsalemink <tim.nicolas@outlook.com>
2026-02-13 15:40:36 +1300

    Move visualisation to visualiser script

--- src/ll_visualiser/min_gui.py
+++ src/ll_visualiser/min_gui.py
@@ -1,9 +1,6 @@
 import os
-import pyvista as pv
 
-from ll_visualiser.utils import visualise_meshes, visualise_landmarks, get_files_by_extension, load_landmarks
-
-pv.global_theme.allow_empty_mesh = True
+from ll_visualiser.visualiser import visualise_model
 
 
 model_directory = "D:\\Projects\\Gait\\example_models\\Models\\Meshes"
@@ -14,18 +11,5 @@ right_predicted_landmark_file = os.path.join(model_directory, 'predicted_lms_rig
 
 
 if __name__ == "__main__":
-    p = pv.Plotter(lighting='light kit', theme=pv.set_plot_theme('default'), window_size=[2560, 1440])
-    p.set_background('#2b2b2b')
-
-    mesh_files = get_files_by_extension(model_directory, ['.ply', '.stl'])
-    left_landmarks = load_landmarks(left_predicted_landmark_file)
-    right_landmarks = load_landmarks(right_predicted_landmark_file)
-
-    visualise_meshes(p, mesh_files)
-    visualise_landmarks(p, left_landmarks, 'left')
-    visualise_landmarks(p, right_landmarks, 'right')
-
-    # Set initial view to frontal view.
-    p.view_zy(negative=True)
-    p.add_axes(labels_off=False)
+    p = visualise_model(model_directory, left_predicted_landmark_file, right_predicted_landmark_file)
     p.show(interactive=True, auto_close=False)
---
+++ src/ll_visualiser/visualiser.py
@@ -0,0 +1,26 @@
+
+import pyvista as pv
+
+from ll_visualiser.utils import visualise_meshes, visualise_landmarks, get_files_by_extension, load_landmarks
+
+
+pv.global_theme.allow_empty_mesh = True
+
+
+def visualise_model(model_directory, left_predicted_landmark_file, right_predicted_landmark_file):
+    p = pv.Plotter(lighting='light kit', theme=pv.set_plot_theme('default'), window_size=[2560, 1440])
+    p.set_background('#2b2b2b')
+
+    mesh_files = get_files_by_extension(model_directory, ['.ply', '.stl'])
+    left_landmarks = load_landmarks(left_predicted_landmark_file)
+    right_landmarks = load_landmarks(right_predicted_landmark_file)
+
+    visualise_meshes(p, mesh_files)
+    visualise_landmarks(p, left_landmarks, 'left')
+    visualise_landmarks(p, right_landmarks, 'right')
+
+    # Set initial view to frontal view.
+    p.view_zy(negative=True)
+    p.add_axes(labels_off=False)
+
+    return p
