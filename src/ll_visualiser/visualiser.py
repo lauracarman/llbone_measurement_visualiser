5558747f22a2b34969ebbcaa0d4ea2474c82a5f1
Tim Salemink <82302710+tsalemink@users.noreply.github.com>
2026-03-04 22:19:07 +1300

    Fix bug in difference calculation

--- src/ll_visualiser/visualiser.py
+++ src/ll_visualiser/visualiser.py
@@ -13,13 +13,12 @@ def visualise_model(plotter, model_directory, left_original_landmark_file, right
     left_predicted_landmarks = load_landmarks(left_predicted_landmark_file)
     right_predicted_landmarks = load_landmarks(right_predicted_landmark_file)
 
-    original_landmarks = {**left_original_landmarks, **right_original_landmarks}
-    predicted_landmarks = {**left_predicted_landmarks, **right_predicted_landmarks}
-    differences = calculate_differences(original_landmarks, predicted_landmarks)
+    left_differences = calculate_differences(left_original_landmarks, left_predicted_landmarks)
+    right_differences = calculate_differences(right_original_landmarks, right_predicted_landmarks)
 
     visualise_meshes(plotter, mesh_files)
-    visualise_landmarks(plotter, left_predicted_landmarks, differences, 'left', 'red')
-    visualise_landmarks(plotter, right_predicted_landmarks, differences, 'right', 'red')
+    visualise_landmarks(plotter, left_predicted_landmarks, left_differences, 'left', 'red')
+    visualise_landmarks(plotter, right_predicted_landmarks, right_differences, 'right', 'red')
     visualise_landmarks_min(plotter, left_original_landmarks, 'left', 'orange')
     visualise_landmarks_min(plotter, right_original_landmarks, 'right', 'orange')
 
